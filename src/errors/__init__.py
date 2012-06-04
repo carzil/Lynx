aebb4e3fcf60e97cf4e2bb4473770a8dce044b10
Alexander Andreev <carzil@yandex.ru>
2012-06-04 10:42:49 +0400

    removed unless modules

--- src/errors/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-#(c) Andreev Alexander (aka Carzil) 2011
-
-version = "0.1a"
--- src/errors/system.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-#(c) Andreev Alexander (aka Carzil) 2011
-M = "It is fatal error. Please contact with Lynx support team for more information!" 
-def Ly_SystemError(msg):
-    print("SystemError:", msg)
-    print(M)
-    
-def Ly_IOError(msg):
-    print("IOError:", msg)
